3183996a57b2e289d132923f42a26858b54e4a18
juang-10 <63117558+juang-10@users.noreply.github.com>
2021-08-05 21:45:27 -0500

    validate that the form fields are not empty

--- js/app.js
+++ js/app.js
@@ -1,13 +1,36 @@
 // Variables
 const btnEnviar = document.querySelector('#enviar');
+const formulario = document.querySelector('#enviar-mail')
+
+// Variables para campos
+const email = document.querySelector('#email');
+const asunto = document.querySelector('#asunto');
+const mensaje = document.querySelector('#mensaje');
 
 eventListeners();
 function eventListeners() {
+    // Cuando la app arranca
     document.addEventListener('DOMContentLoaded', iniciarApp);
+
+    // Campos del formulaio
+    email.addEventListener('blur', validarFormulario);
+    asunto.addEventListener('blur', validarFormulario);
+    mensaje.addEventListener('blur', validarFormulario);
 }
 
 // Funciones 
 function iniciarApp() {
     btnEnviar.disabled = true;
     btnEnviar.classList.add('cursor-not-allowed', 'opacity-50')
+}
+
+// Valida el formulario
+function validarFormulario(e) {
+    if(e.target.value.length > 0){
+        console.log('Sí hay algo');
+    } else {
+        e.target.style.borderBottomColor = 'red';
+        e.target.classList.add('border', 'border-red-500');
+        mostrarError();
+    }
 }
